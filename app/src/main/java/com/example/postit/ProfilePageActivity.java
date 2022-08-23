012c3ace0c98af06d31fca0f88725ffbff38c14a
idanbarshlomo <idan6789@gmail.com>
2022-08-24 01:32:12 +0300

    Add user profile page connection to fire store

--- app/src/main/java/com/example/postit/ProfilePageActivity.java
+++ app/src/main/java/com/example/postit/ProfilePageActivity.java
@@ -1,12 +1,20 @@
 package com.example.postit;
 
+import static com.example.postit.FirestoreUtils.getFirestore;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.postit.entities.Users;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 public class ProfilePageActivity extends AppCompatActivity {
 
@@ -15,6 +23,13 @@ public class ProfilePageActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        TextView firstnameTextView = findViewById(R.id.FirstnameProfilePage);
+        TextView addressTextView = findViewById(R.id.AddressProfilePage);
+        TextView mobileTextView = findViewById(R.id.mobileProfilePage);
+        TextView dateofbirthTextView = findViewById(R.id.DOBProfilePage);
+        TextView emailTextView = findViewById(R.id.emailProfilePage);
+        emailTextView.setText(FirebaseUtils.getCurrentemail());
 
         ImageButton profile = findViewById(R.id.ProfileButton);
         profile.setVisibility(View.INVISIBLE);
@@ -28,7 +43,7 @@ public class ProfilePageActivity extends AppCompatActivity {
             public void onClick(View view) {
                 finish();
             }
-    });
+        });
         Button logoutBtn=findViewById(R.id.logout);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +51,28 @@ public class ProfilePageActivity extends AppCompatActivity {
                 FirebaseUtils.logout(ProfilePageActivity.this);
             }
         });
+
+        getFirestore().collection("users").document(FirebaseUtils.getCurrentemail()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (!documentSnapshot.exists()) {
+                            return;
+                        } else {
+                            Users user = documentSnapshot.toObject(Users.class);
+                            dateofbirthTextView.setText(user.getDateOfBirth());
+                            addressTextView.setText(user.getAddress());
+                            mobileTextView.setText(user.getMobileNumber());
+                            firstnameTextView.setText(user.getFirstName());
+                    }
+                }})
+            .addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception e) {
+//                                    Toast.makeText(getApplicationContext(), "Error getting data!!!", Toast.LENGTH_LONG).show();
+           // Log.e(TAG, "onFailure: " + e);
+        }
+    });
+
     }
 }
---
+++ app/src/main/java/com/example/postit/entities/Users.java
@@ -0,0 +1,50 @@
+package com.example.postit.entities;
+
+public class Users {
+
+    private String FirstName;
+    private String LastName;
+    private String Address;
+    private String MobileNumber;
+    private String DateOfBirth;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getMobileNumber() {
+        return MobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        MobileNumber = mobileNumber;
+    }
+
+    public String getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        DateOfBirth = dateOfBirth;
+    }
+}
