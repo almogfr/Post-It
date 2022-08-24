445b46e77f7ef2895eee263919d69b6c122fb107
idanbarshlomo <idan6789@gmail.com>
2022-08-24 23:51:29 +0300

    Add view post page functionality

--- app/src/main/java/com/example/postit/ImageClickedActivity.java
+++ app/src/main/java/com/example/postit/ImageClickedActivity.java
@@ -1,22 +1,52 @@
 package com.example.postit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ImageClickedActivity extends AppCompatActivity {
+import com.example.postit.entities.Post;
+
+import java.io.Serializable;
+
+public class ImageClickedActivity extends AppCompatActivity implements Serializable {
 
     ImageView selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_clicked);
-        selectedImage = (ImageView) findViewById(R.id.selectedImage);
-        Bundle extras = getIntent().getExtras();
-        String temp = extras.getString("image_item", "");
-        new ImageDownloader(temp, selectedImage).execute();
-//        selectedImage.setImageResource(temp);
+
+        Intent mIntent = getIntent();
+        Post postItems = (Post) mIntent.getSerializableExtra("UniqueKey");
+
+        TextView postName = findViewById(R.id.feed_post_name);
+        TextView postWhen = findViewById(R.id.feed_post_when);
+        ImageView feedPostProfileImg = findViewById(R.id.feed_post_profile_img);
+        ImageView feedPostImg = findViewById(R.id.feed_post_img);
+        TextView postLikes = findViewById(R.id.post_likes);
+
+        postName.setText(postItems.getName());
+        postWhen.setText(postItems.getWhenPosted());
+        new ImageDownloader(postItems.getProfileImage(), feedPostProfileImg).execute();
+        new ImageDownloader(postItems.getImgUrl(), feedPostImg).execute();
+//        postLikes.setText(postItems.getLikes());
+
+        ImageButton prfilePageButton = (ImageButton) findViewById(R.id.ProfileButton);
+        prfilePageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ImageClickedActivity.this, ProfilePageActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton BackButton = (ImageButton) findViewById(R.id.BackButton);
+        BackButton.setOnClickListener(view -> finish());
     }
 }
